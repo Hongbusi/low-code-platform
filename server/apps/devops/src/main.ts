4f6b00363672e28ef54f7eb9e29f4e50198986c6
Hongbusi <coderhbs@gmail.com>
2023-11-25 21:48:47 +0800

    chore(server): update

--- server/apps/devops/src/main.ts
+++ server/apps/devops/src/main.ts
@@ -3,6 +3,6 @@ import { DevopsModule } from './devops.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(DevopsModule);
-  await app.listen(3000);
+  await app.listen(3001);
 }
 bootstrap();
